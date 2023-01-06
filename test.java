ef5a334fcbd0ee4d66f52e98195b2ec31317d2e8
sahanagnxworkz <113198896+sahanagnxworkz@users.noreply.github.com>
2023-01-06 13:28:45 +0530

    test

---
+++ test.java
@@ -0,0 +1,47 @@
+3.generalized class & special class
+generalized classes are declared by developers
+special classes are already declared magical packages
+
+4.
+A-->by declaring class as private
+B-->String class
+C-->association
+
+5.
+Comparable and Comparator
+
+6.
+ArrayList
+1.it is index based
+2.fast to assesing values
+3.modification is easy and fast while compiling
+
+LinkedList
+1.it is Node based
+2.every node holds address of previous data and next data
+3.modification is bit slow comparing with array list
+
+7.iterator and ListIterator
+
+List :
+Iterator:
+iterator is an inteface 
+In list we can access only from 0th index and we can do only remove operation in iterator
+
+listIteraror 
+ 
+in listIteraror we can access& modify the data through any index
+
+10.
+static polymorphism 
+it's achieved by methodOverloading
+methodOverloading means same methods with vary in parameter variables
+and sequence of parameter should be change
+
+dynamic polymorphism
+it's achieved by method overriding through inheritance
+method overriding means subClass overrides the parent members 
+
+11. by achieving encapsulation 
+declaring properties has private and declare a getter and setters
+ 
